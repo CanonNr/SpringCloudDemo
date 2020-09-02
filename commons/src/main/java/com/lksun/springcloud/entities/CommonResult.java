31de9268066e8f55a39f5e537f2b3f0ed78a3eef
CanonNr <717437709@qq.com>
2020-09-02 22:37:42 +0800

    first commit

---
+++ commons/src/main/java/com/lksun/springcloud/entities/CommonResult.java
@@ -0,0 +1,37 @@
+package com.lksun.springcloud.entities;
+
+public class CommonResult {
+    private Integer code;
+    private String message;
+    private Object data;
+
+    public CommonResult(Integer code, String message, Object data){
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
---
+++ user-9001/src/main/java/com/lksun/springcloud/UserMain9001.java
@@ -0,0 +1,10 @@
+package com.lksun.springcloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(UserMain9001.class, args);
+    }
+}
