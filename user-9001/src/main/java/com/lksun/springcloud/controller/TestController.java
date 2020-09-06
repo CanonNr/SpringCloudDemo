94ef60036a255bae461dca01b2e59116439aaea5
CanonNr <717437709@qq.com>
2020-09-06 11:30:25 +0800

    http接口添加参数

--- user-9001/src/main/java/com/lksun/springcloud/controller/TestController.java
+++ user-9001/src/main/java/com/lksun/springcloud/controller/TestController.java
@@ -3,6 +3,7 @@ package com.lksun.springcloud.controller;
 import com.lksun.springcloud.service.GetUser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.lksun.springcloud.entities.CommonResult;
 
@@ -12,8 +13,8 @@ public class TestController {
     public String port;
 
     @RequestMapping("/user")
-    public CommonResult get(){
+    public CommonResult get(@RequestParam(value = "id",defaultValue="1") Integer id){
         GetUser user = new GetUser();
-        return new CommonResult(200,this.port,user.getUserById(1));
+        return new CommonResult(200,this.port,user.getUserById(id));
     }
 }
